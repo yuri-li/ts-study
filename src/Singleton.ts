f59fe8f650b7c91349fb4ee19434469691d76750
yuri-li <javaliyuanyuan@163.com>
2022-03-02 14:23:36 +0800

    init

--- src/Singleton.ts
+++
@@ -1,6 +0,0 @@
-import random from "random"
-class Singleton {
-    randomNumber = random.int(1, 100)
-}
-const singleton = new Singleton()
-export { singleton }
--- src/hello.ts
+++
@@ -1,9 +0,0 @@
-const message = "hello world"
-function sayHello(username: string) {
-    return `hello ${username}`
-}
-const admin = {
-    username: "admin",
-    password: "admin",
-}
-export { message, sayHello, admin, }
